033d1738415d9931de633194d27d352a904c7b03
Justin Webster <justin.webster@gmail.com>
2021-12-03 17:29:26 -0800

    issue-644: multi-AGW: Added a new, optional `resource_group_name` keyword param to the `AzureClient.get_application_gateway_by_name` method.

--- src/bosh_azure_cpi/spec/unit/azure_client/get_operation_spec.rb
+++ src/bosh_azure_cpi/spec/unit/azure_client/get_operation_spec.rb
@@ -661,20 +661,40 @@ describe Bosh::AzureCloud::AzureClient do
         ).to be_nil
       end
 
-      it 'should return the resource if response body is not null' do
-        stub_request(:get, application_gateway_uri).to_return(
-          status: 200,
-          body: application_gateway_response_body,
-          headers: {}
-        )
-        stub_request(:get, public_ip_uri).to_return(
-          status: 200,
-          body: public_ip_response_body.to_json,
-          headers: {}
-        )
-        expect(
-          azure_client.get_application_gateway_by_name(application_gateway_name)
-        ).to eq(fake_application_gateway)
+      context 'when resource_group_name is not specified' do
+        it 'should return the resource if response body is not null' do
+          stub_request(:get, application_gateway_uri).to_return(
+            status: 200,
+            body: application_gateway_response_body,
+            headers: {}
+          )
+          stub_request(:get, public_ip_uri).to_return(
+            status: 200,
+            body: public_ip_response_body.to_json,
+            headers: {}
+          )
+          expect(
+            azure_client.get_application_gateway_by_name(application_gateway_name)
+          ).to eq(fake_application_gateway)
+        end
+      end
+
+      context 'when resource_group_name is specified' do
+        it 'should return the resource if response body is not null' do
+          stub_request(:get, application_gateway_uri).to_return(
+            status: 200,
+            body: application_gateway_response_body,
+            headers: {}
+          )
+          stub_request(:get, public_ip_uri).to_return(
+            status: 200,
+            body: public_ip_response_body.to_json,
+            headers: {}
+          )
+          expect(
+            azure_client.get_application_gateway_by_name(application_gateway_name, resource_group_name: default_resource_group_name)
+          ).to eq(fake_application_gateway)
+        end
       end
     end
   end
