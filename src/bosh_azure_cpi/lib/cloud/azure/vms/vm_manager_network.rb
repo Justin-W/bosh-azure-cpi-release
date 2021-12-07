e604d5698db24c0b96ce9f494166e5220bef7457
Justin Webster <justin.webster@gmail.com>
2021-12-07 12:15:10 -0800

    issue-644: multi-AGW: Refactored the `AzureClient.get_application_gateway_by_name` method to make the `resource_group_name` param a positional param (instead of an optional keyword param).
    Note: This change was made so that the `AzureClient.get_application_gateway_by_name` method's signature is consistent with the signatures of the other `AzureClient.get_*` methods in requiring the `resource_group_name` param to be passed as the first positional param.

--- src/bosh_azure_cpi/lib/cloud/azure/vms/vm_manager_network.rb
+++ src/bosh_azure_cpi/lib/cloud/azure/vms/vm_manager_network.rb
@@ -110,7 +110,7 @@ module Bosh::AzureCloud
         # TODO: issue-644: multi-AGW: Add support for multiple ApplicationGateways
         # TODO: issue-644: multi-BEPool-AGW: Add support for multiple ApplicationGateway Backend Address Pools
         application_gateways = application_gateway_configs.map do |application_gateway_config|
-          application_gateway = @azure_client.get_application_gateway_by_name(application_gateway_config.name, resource_group_name: application_gateway_config.resource_group_name)
+          application_gateway = @azure_client.get_application_gateway_by_name(application_gateway_config.resource_group_name, application_gateway_config.name)
           cloud_error("Cannot find the application gateway '#{application_gateway_config.name}'") if application_gateway.nil?
           application_gateway
         end
