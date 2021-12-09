2bdf8e31caca0d87e39d93c13514093fff80a89f
Justin Webster <justin.webster@gmail.com>
2021-12-09 15:18:58 -0800

    ... multi-pool-AGW specs

--- src/bosh_azure_cpi/spec/unit/vm_manager/create/dynamic_public_ip_spec.rb
+++ src/bosh_azure_cpi/spec/unit/vm_manager/create/dynamic_public_ip_spec.rb
@@ -361,7 +361,7 @@ describe Bosh::AzureCloud::VMManager do
                   it 'adds the public IP to the default pool' do
                     _, vm_params = vm_manager_for_pip.create(bosh_vm_meta, location, vm_props, disk_cids, network_configurator, env, agent_util, network_spec, config)
                     expect(vm_params[:name]).to eq(vm_name)
-                    # TODO: Add more expectations here? The expects above only verify that the VM was created, but not that the IP was assigned to the correct pool.
+                    # TODO: Add more expectations here? The expects above only verify that the VM was created, but not that the IP was assigned to the correct pool(s).
                   end
                 end
 
@@ -383,7 +383,7 @@ describe Bosh::AzureCloud::VMManager do
                   it 'adds the public IP to the specified pool' do
                     _, vm_params = vm_manager_for_pip.create(bosh_vm_meta, location, vm_props, disk_cids, network_configurator, env, agent_util, network_spec, config)
                     expect(vm_params[:name]).to eq(vm_name)
-                    # TODO: Add more expectations here? The expects above only verify that the VM was created, but not that the IP was assigned to the correct pool.
+                    # TODO: Add more expectations here? The expects above only verify that the VM was created, but not that the IP was assigned to the correct pool(s).
                   end
                 end
               end
